4487c044fc5edd6d7e97bfcb84b562d4802bc2e6
Chris <c.r.zambito@gmail.com>
2023-03-01 12:19:36 -0500

    fix feature 6

--- todos/models.py
+++ todos/models.py
@@ -8,13 +8,23 @@ class TodoList(models.Model):
     name = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class TodoItem(models.Model):
     task = models.CharField(max_length=100)
-    due_date = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    due_date = models.DateTimeField(
+        default=timezone.now,
+        null=True,
+        blank=True
+        )
     is_completed = models.BooleanField(default=False)
     list = models.ForeignKey(
         TodoList,
         on_delete=models.CASCADE,
         related_name='items',
         )
+    
+    def __str__(self):
+        return self.task
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from todos.models import TodoList
 
 # Create your views here.
+
+
+def todo_list
