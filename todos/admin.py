eff6d20664010593c283a187e84894efa097427a
Chris <c.r.zambito@gmail.com>
2023-03-01 11:16:45 -0500

    Feature 4 Complete

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from todo.models import TodoList
 
 # Register your models here.
+
+
+@admin.register(TodoList)
+class TodoList(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'id',
+    )
